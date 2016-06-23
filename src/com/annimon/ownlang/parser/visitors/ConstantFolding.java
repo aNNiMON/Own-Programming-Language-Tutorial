8c5090d9d294ea8641883721efe0029294bbc5d4
Victor <melnikukr@mail.ru>
2016-06-23 22:44:20 +0300

    Добавлены оптимизаторы-посетители

---
+++ src/com/annimon/ownlang/parser/visitors/ConstantFolding.java
@@ -0,0 +1,87 @@
+package com.annimon.ownlang.parser.visitors;
+
+import com.annimon.ownlang.exceptions.OperationIsNotSupportedException;
+import com.annimon.ownlang.parser.Optimizer;
+import com.annimon.ownlang.parser.ast.BinaryExpression;
+import com.annimon.ownlang.parser.ast.ConditionalExpression;
+import com.annimon.ownlang.parser.ast.Node;
+import com.annimon.ownlang.parser.ast.UnaryExpression;
+import com.annimon.ownlang.parser.ast.ValueExpression;
+
+/**
+ * Performs constant folding optimization.
+ */
+public class ConstantFolding extends OptimizationVisitor<Void> implements Optimizer.Info {
+
+    private int binaryExpressionFoldingCount;
+    private int conditionalExpressionFoldingCount;
+    private int unaryExpressionFoldingCount;
+
+    public ConstantFolding() {
+        binaryExpressionFoldingCount = 0;
+        conditionalExpressionFoldingCount = 0;
+        unaryExpressionFoldingCount = 0;
+    }
+
+    @Override
+    public int optimizationsCount() {
+        return binaryExpressionFoldingCount
+                + conditionalExpressionFoldingCount
+                + unaryExpressionFoldingCount;
+    }
+
+    @Override
+    public String summaryInfo() {
+        if (optimizationsCount() == 0) return "";
+        final StringBuilder sb = new StringBuilder();
+        if (binaryExpressionFoldingCount > 0) {
+            sb.append("\nBinaryExpression foldings: ").append(binaryExpressionFoldingCount);
+        }
+        if (conditionalExpressionFoldingCount > 0) {
+            sb.append("\nConditionalExpression foldings: ").append(conditionalExpressionFoldingCount);
+        }
+        if (unaryExpressionFoldingCount > 0) {
+            sb.append("\nUnaryExpression foldings: ").append(unaryExpressionFoldingCount);
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public Node visit(BinaryExpression s, Void t) {
+        if ( (s.expr1 instanceof ValueExpression) && (s.expr2 instanceof ValueExpression) ) {
+            binaryExpressionFoldingCount++;
+            try {
+                return new ValueExpression(s.eval());
+            } catch (OperationIsNotSupportedException op) {
+                binaryExpressionFoldingCount--;
+            }
+        }
+        return super.visit(s, t);
+    }
+
+    @Override
+    public Node visit(ConditionalExpression s, Void t) {
+        if ( (s.expr1 instanceof ValueExpression) && (s.expr2 instanceof ValueExpression) ) {
+            conditionalExpressionFoldingCount++;
+            try {
+                return new ValueExpression(s.eval());
+            } catch (OperationIsNotSupportedException op) {
+                conditionalExpressionFoldingCount--;
+            }
+        }
+        return super.visit(s, t);
+    }
+
+    @Override
+    public Node visit(UnaryExpression s, Void t) {
+        if (s.expr1 instanceof ValueExpression) {
+            unaryExpressionFoldingCount++;
+            try {
+                return new ValueExpression(s.eval());
+            } catch (OperationIsNotSupportedException op) {
+                unaryExpressionFoldingCount--;
+            }
+        }
+        return super.visit(s, t);
+    }
+}

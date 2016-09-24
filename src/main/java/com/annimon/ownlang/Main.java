e579cfcf9575956434842e378c9189577e57a2a9
Victor <annimon119@gmail.com>
2016-09-24 15:46:12 +0300

    Версия 1.3.0

--- src/main/java/com/annimon/ownlang/Main.java
+++ src/main/java/com/annimon/ownlang/Main.java
@@ -21,7 +21,7 @@ import java.util.concurrent.TimeUnit;
  */
 public final class Main {
     
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
 
     private static String[] ownlangArgs = new String[0];
 

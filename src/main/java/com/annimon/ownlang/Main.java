7b8c6ad40684fcac116e19a11af9693680ee61d2
Victor <annimon119@gmail.com>
2016-12-07 15:04:59 +0200

    Генерируемая дата сборки в версии приложения

--- src/main/java/com/annimon/ownlang/Main.java
+++ src/main/java/com/annimon/ownlang/Main.java
@@ -22,7 +22,7 @@ import java.util.concurrent.TimeUnit;
  */
 public final class Main {
     
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.3.0_" + Gen.BUILD_DATE;
 
     private static String[] ownlangArgs = new String[0];
 

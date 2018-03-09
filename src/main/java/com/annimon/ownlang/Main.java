a4d5b43003aa3f8bef892e3bf4eefb589ae81576
Victor <annimon119@gmail.com>
2018-03-09 17:12:43 +0200

    Обновлена конфигурация сборки, обновлены библиотеки

--- src/main/java/com/annimon/ownlang/Main.java
+++ src/main/java/com/annimon/ownlang/Main.java
@@ -22,7 +22,7 @@ import java.util.concurrent.TimeUnit;
  */
 public final class Main {
     
-    public static final String VERSION = "1.3.0_" + Gen.BUILD_DATE;
+    public static final String VERSION = "1.3.1_" + Gen.BUILD_DATE;
 
     private static String[] ownlangArgs = new String[0];
 

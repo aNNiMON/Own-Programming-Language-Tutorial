da950f96c25e60122f1beec2d3e2913bff6c08c6
aNNiMON <annimon119@gmail.com>
2023-09-16 22:22:29 +0300

    Better explaining parse errors

--- ownlang-parser/src/main/java/com/annimon/ownlang/exceptions/ParseException.java
+++ ownlang-parser/src/main/java/com/annimon/ownlang/exceptions/ParseException.java
@@ -1,16 +1,35 @@
 package com.annimon.ownlang.exceptions;
 
+import com.annimon.ownlang.parser.Pos;
+
 /**
  *
  * @author aNNiMON
  */
 public final class ParseException extends RuntimeException {
-    
-    public ParseException() {
-        super();
+
+    private final Pos start;
+    private final Pos end;
+
+    public ParseException(String message) {
+        this(message, Pos.ZERO, Pos.ZERO);
     }
-    
-    public ParseException(String string) {
-        super(string);
+
+    public ParseException(String message, Pos pos) {
+        this(message, pos, pos);
+    }
+
+    public ParseException(String message, Pos start, Pos end) {
+        super(message);
+        this.start = start;
+        this.end = end;
+    }
+
+    public Pos getStart() {
+        return start;
+    }
+
+    public Pos getEnd() {
+        return end;
     }
 }

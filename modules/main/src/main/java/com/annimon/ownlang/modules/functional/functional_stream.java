d643f596a8cfc0d6e8ff2820def076c4e6bb6811
aNNiMON <annimon119@gmail.com>
2023-10-05 16:32:18 +0300

    [functional] Deny varargs input argument for stream

--- modules/main/src/main/java/com/annimon/ownlang/modules/functional/functional_stream.java
+++ modules/main/src/main/java/com/annimon/ownlang/modules/functional/functional_stream.java
@@ -9,18 +9,11 @@ public final class functional_stream implements Function {
     public Value execute(Value[] args) {
         Arguments.checkAtLeast(1, args.length);
 
-        if (args.length > 1) {
-            return new StreamValue(new ArrayValue(args));
-        }
-
         final Value value = args[0];
-        switch (value.type()) {
-            case Types.MAP:
-                return new StreamValue(((MapValue) value).toPairs());
-            case Types.ARRAY:
-                return new StreamValue((ArrayValue) value);
-            default:
-                throw new TypeException("Invalid argument. Array or map expected");
-        }
+        return switch (value.type()) {
+            case Types.MAP -> new StreamValue(((MapValue) value).toPairs());
+            case Types.ARRAY -> new StreamValue((ArrayValue) value);
+            default -> throw new TypeException("Invalid argument. Array or map expected");
+        };
     }
 }

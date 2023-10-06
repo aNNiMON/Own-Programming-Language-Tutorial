c3b0c6f5304d66da337b3c0d65786dc1816a5749
aNNiMON <annimon119@gmail.com>
2023-10-06 20:08:07 +0300

    Show location of the closest error from call stack

--- ownlang-core/src/main/java/com/annimon/ownlang/stages/StagesData.java
+++ ownlang-core/src/main/java/com/annimon/ownlang/stages/StagesData.java
@@ -1,5 +1,7 @@
 package com.annimon.ownlang.stages;
 
+import java.util.function.Supplier;
+
 public interface StagesData {
 
     <T> T get(String tag);
@@ -7,6 +9,11 @@ public interface StagesData {
     default <T> T getOrDefault(String tag, T other) {
         T value = get(tag);
         return value != null ? value : other;
+    }
+
+    default <T> T getOrDefault(String tag, Supplier<? extends T> otherSuppler) {
+        T value = get(tag);
+        return value != null ? value : otherSuppler.get();
     }
 
     void put(String tag, Object input);

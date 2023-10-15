66ac44e834ad0292f74a6ff577a10d71539e69cf
aNNiMON <annimon119@gmail.com>
2023-10-15 23:09:36 +0300

    Source located VariableDoesNotExistsException

--- ownlang-parser/src/main/java/com/annimon/ownlang/parser/ast/VariableExpression.java
+++ ownlang-parser/src/main/java/com/annimon/ownlang/parser/ast/VariableExpression.java
@@ -3,17 +3,29 @@ package com.annimon.ownlang.parser.ast;
 import com.annimon.ownlang.exceptions.VariableDoesNotExistsException;
 import com.annimon.ownlang.lib.ScopeHandler;
 import com.annimon.ownlang.lib.Value;
+import com.annimon.ownlang.util.Range;
+import com.annimon.ownlang.util.SourceLocation;
 
 /**
  *
  * @author aNNiMON
  */
-public final class VariableExpression extends InterruptableNode implements Accessible {
+public final class VariableExpression extends InterruptableNode implements Accessible, SourceLocation {
     
     public final String name;
+    private Range range;
     
     public VariableExpression(String name) {
         this.name = name;
+    }
+
+    public void setRange(Range range) {
+        this.range = range;
+    }
+
+    @Override
+    public Range getRange() {
+        return range;
     }
 
     @Override
@@ -25,7 +37,7 @@ public final class VariableExpression extends InterruptableNode implements Acces
     @Override
     public Value get() {
         if (!ScopeHandler.isVariableOrConstantExists(name)) {
-            throw new VariableDoesNotExistsException(name);
+            throw new VariableDoesNotExistsException(name, getRange());
         }
         return ScopeHandler.getVariableOrConstant(name);
     }

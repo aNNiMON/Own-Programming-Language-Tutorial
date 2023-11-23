68570f10f1ba0fb00f1aae651a25c9fa4b954eb2
aNNiMON <annimon119@gmail.com>
2023-11-24 00:13:13 +0200

    Add custom components, update vuepress version

---
+++ docs/docs/.vuepress/components/Since.vue
@@ -0,0 +1,32 @@
+<template>
+  <span class="since">{{ text }} <b>{{ version }}</b></span>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    version: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.since {
+  display: inline-block;
+  font-size: 13px;
+  height: 18px;
+  line-height: 18px;
+  border-radius: 3px;
+  padding: 0 4px;
+  color: var(--c-bg);
+  background-color: var(--usc-since);
+}
+</style>

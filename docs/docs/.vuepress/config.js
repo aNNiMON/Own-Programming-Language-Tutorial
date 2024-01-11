3ba96aa99b3792bc8f1e35745d33cfacea35473d
aNNiMON <annimon119@gmail.com>
2024-01-11 22:12:14 +0200

    Add search plugin, build docs task

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -2,6 +2,7 @@ import { defineUserConfig, defaultTheme } from 'vuepress'
 import { getDirname, path } from '@vuepress/utils'
 import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 import { prismjsPlugin } from '@vuepress/plugin-prismjs'
+import { searchPlugin } from '@vuepress/plugin-search'
 import { sidebarConfig } from './configs/sidebar'
 import { navbarConfig } from './configs/navbar'
 import Prism from 'prismjs';
@@ -11,6 +12,7 @@ definePrismOwnLang(Prism)
 const __dirname = getDirname(import.meta.url)
 
 export default defineUserConfig({
+  base: "/docs/ownlang/",
   locales: {
     '/en/': {
       lang: 'en-US',
@@ -50,6 +52,12 @@ export default defineUserConfig({
     }),
     registerComponentsPlugin({
       componentsDir: path.resolve(__dirname, './components'),
-    })
+    }),
+    searchPlugin({
+      locales: {
+        '/en/': { placeholder: 'Search' },
+        '/ru/': { placeholder: 'Поиск' },
+      },
+    }),
   ],
 })
